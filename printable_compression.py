d675ffd2e70cfd6f508a0f8c8fb157ba5ba14fdc
David Cary <d.cary@ieee.org>
2025-07-05 14:23:58 -0700

    boilerplate for new python ".py" file

---
+++ printable_compression.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+printable_compression.py
+
+2025-07-01: started by David Cary
+"""
+
+
+def test():
+    pass
+
+if __name__ == "__main__":
+    test()
+
+
+# modeline as recommended by https://wiki.python.org/moin/Vim
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
